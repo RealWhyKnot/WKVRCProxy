11d116ed548555062808e5b76d1034588c9b55a8
WhyKnot <github.idealness@pass.webmailroom.net>
2026-04-21 09:39:40 -0500

    chore: bump version to 2026.4.21.0-77A8 (appStore.ts) (2026.4.21.0-77A8)

--- src/WKVRCProxy.UI/ui/src/stores/appStore.ts
+++ src/WKVRCProxy.UI/ui/src/stores/appStore.ts
@@ -139,7 +139,7 @@ export const useAppStore = defineStore('app', () => {
   const cloudResolveError = ref('')
   
   const isBridgeReady = ref(false)
-  const version = ref('2026.4.18.4-B60B')
+  const version = ref('2026.4.21.0-77A8')
 
   const bypassMemory = ref<BypassMemoryRow[]>([])
   const ytDlpUpdate = ref<YtDlpUpdateStatus>({
@@ -345,6 +345,7 @@ export const useAppStore = defineStore('app', () => {
     refreshYtDlpUpdate
   }
 })
+
 
 
 
